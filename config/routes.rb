f456107a66acd991091f31e8a6d3f52c7fba625f
Azzerty23 <azzerty23@hotmail.fr>
2019-10-31 00:35:32 +0100

    feat: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :gossips
+
+  root 'gossips#index'
+
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :gossips do
+    resources :comments, :likes
+  end
+  resources :city, only: [:index, :show]
+
 end
